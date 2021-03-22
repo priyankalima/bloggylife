5f7a9db90eea9da4600ff0c74914155c01a8961f
priyankalima <53181160+priyankalima@users.noreply.github.com>
2021-03-22 12:41:02 +0530

    success

--- src/components/Blogger.jsx
+++ src/components/Blogger.jsx
@@ -160,24 +160,24 @@ return(
              <h3>My Plans</h3>
              <div className="row plan">
                  <div className="col-sm-6 col-md-6 col-lg-6  ">
-                 <div class="card" style={{width: '20rem'}}>
+                 <center><div class="card" style={{width: '25rem'}}>
                  <img src="http://socialmediaweek.org/wp-content/blogs.dir/1/files/2015/07/Writing-freelancer.jpg" class="card-img-top" alt="..."/>
                   <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                  </div>
+                  </div></center>
                  </div>
                  <div className="col-sm-6 col-md-6 col-lg-6 ">
-                 <div class="card" style={{width: '20rem'}}>
+                 <center><div class="card" style={{width: '25rem'}}>
                  <img src="http://socialmediaweek.org/wp-content/blogs.dir/1/files/2015/07/Writing-freelancer.jpg" class="card-img-top" alt="..."/>
                   <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                  </div>
+                  </div></center>
                  </div>
              </div>
          </div>
