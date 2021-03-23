e3889df142e56769ab3d653b6a6463d554465b32
priyankalima <53181160+priyankalima@users.noreply.github.com>
2021-03-23 11:27:51 +0530

    saved changes

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -7,7 +7,7 @@ const Stories =()=>{
         <>
         <div className="articleBackground">
             <h4 className="title">Stories</h4>
-            <p className="text">yes this is really great</p>
+            <p className="text">What stories do you want ? well there is lots of story that might be feel Interestring just try to read..:) </p>
         </div>
         </>
     )
